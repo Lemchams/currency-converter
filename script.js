c22c74f7113cf4e3e4ea783c834b710924342daa
Lemchams <chamslemamou@gmail.com>
2026-07-30 16:51:01 +0000

    Start script

---
+++ script.js
@@ -0,0 +1,42 @@
+const converterForm = document.getElementById("converter-form");
+const fromCurrency = document.getElementById("from-currency");
+const toCurrency = document.getElementById("to-currency");
+const amountInput = document.getElementById("amount");
+const resultDiv = document.getElementById("result");
+
+
+window.addEventListener("load", fetchCurrencies);
+
+converterForm.addEventListener("submit", convertCurrency);
+
+function fetchCurrencies () {
+    // 
+    const response = await fetch("https://api.exchangerate-api.com/v4/latest/USD");
+    const data = await response.json();
+
+    console.log(data);
+    const currencyOptions = Object.keys(data.rates);
+
+    currencyOptions.forEach(currency => {
+        const option1 = document.createElement("option")
+        option1.value = currency;
+        option1.textContent = currency;
+        fromCurrency.appendChild(option1);
+
+        const option2 = document.createElement("option")
+        option2.value = currency;
+        option2.textContent = currency;
+        toCurrency.appendChild(option2);
+
+    })
+
+
+}
+
+function convertCurrency(e) {
+    e.preventDefault();
+
+    const amount = parseFloat(amountInput.value);
+    const fromCurrencyValue = fromCurrency.value;
+    const toCurrencyValue = toCurrency.value;
+}
